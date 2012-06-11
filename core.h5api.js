95ac5727ed5ceb3a7ec65b5316ee7c5eff9cdb06
Kyle Simpson <getify@gmail.com>
2012-06-11 08:32:33 -0500

    adding some stub files

---
+++ core.h5api.js
@@ -0,0 +1,9 @@
+/*! core.H5api.js | (c) Kyle Simpson | MIT License: http://getify.mit-license.org */
+
+(function(global){
+
+	global.H5 = {
+		// TODO: fill in core API functions
+	};
+
+})(this);
---
+++ xhr.h5api.js
@@ -0,0 +1,11 @@
+/*! xhr.H5api.js | (c) Kyle Simpson | MIT License: http://getify.mit-license.org */
+
+(function(H5){
+
+	if (!H5) throw new Error("xhr.H5api: core.H5api required but missing.");
+
+	H5.xhr = function(opts) {
+		// TODO: fill in how xhr.H5api works
+	};
+
+})(this.H5);
