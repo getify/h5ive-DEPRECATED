45a267dafc560c327510d288302f0431c28aa890
Alex Lee <aintaer@gmail.com>
2012-07-24 17:25:37 -0400

    Worker example, added the case for inside a Worker

--- example-workers.js
+++ example-workers.js
@@ -1,14 +1,27 @@
-H5
+h5
 .worker('/example/worker.js')
 .send(data)
 .error(function(event) {
-  console.log(event.message);
-  console.log(event.filename);
-  console.log(event.lineno);
+  console.error(event.message + " in " + event.filename + " at line " + event.lineno);
   event.preventDefault();
 });
 .receive(function(data) {
   console.log(data);
-  console.log(this.__raw__.data === JSON.stringify(data));
 })
 .abort();
+
+// Inside the Worker
+importScripts("core.h5ive.js", "worker.h5ive.js");
+
+h5
+.worker(self)
+.receive(function(data) {
+  var sqrt;
+  console.log(data);
+  sqrt = Math.sqrt(data);
+
+  console.log(h5.worker(self) === this);
+  this.send(sqrt);
+  this.__raw__.postMessage(sqrt);
+});
+
