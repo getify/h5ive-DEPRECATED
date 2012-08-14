533c8001eb0fe482c12c8051793bcc57a779f2f4
Nathaniel Lee <code@nathanielklee.com>
2012-08-13 23:47:18 -0400

    If we're not dealing with DOM events on a video tag that significantly reduces the API. Not sure if I'm understanding the goal of the project correctly... maybe I'm just looking at it from the wrong perspective...

--- example-video.js
+++ example-video.js
@@ -3,49 +3,27 @@ var video, playButton;
 video = h5.video({
 	width: 640,
 	height: 360,
-	src: [{ src: 'http://mediapm.edgesuite.net/osmf/content/test/spacealonehd_sounas_640_700.mp4', type: 'video/mp4' }],
+	src: [
+		{ src: 'videos/my_really_cool_video.mp4', type: 'video/mp4' },
+		{ src: 'videos/my_really_cool_video.ogv', type: 'video/ogg' },
+		{ src: 'videos/my_really_cool_video.webm', type: 'video/webm' }
+	],
 	poster: 'images/my_really_cool_image.png',
 	controls: true,
-	preload: 'autoplay'
+	preload: 'preload',
+	autoplay: true,
+	muted: true,
+	loop: true
 });
 
 document.body.appendChild(video.create());
 
 video
-	.onLoadStart(logEvent)
-	.onProgress(logEvent)
-	.onSuspend(logEvent)
-	.onAbort(logEvent)
-	.onError(logEvent)
-	.onEmptied(logEvent)
-	.onStalled(logEvent)
-	.onLoadedMetaData(logEvent)
-	.onLoadedData(logEvent)
-	.onCanPlay(logEvent)
-	.onCanPlayThrough(logEvent)
-	.onPlaying(logEvent)
-	.onWaiting(logEvent)
-	.onSeeking(logEvent)
-	.onSeeked(logEvent)
-	.onEnded(logEvent)
-	.onDurationChange(logEvent)
-	.onTimeUpdate(logEvent)
-	.onPlay(logEvent)
-	.onPause(logEvent)
-	.onRateChange(logEvent)
-	.onVolumeChange(logEvent);
-
-playButton = document.createElement('button');
-playButton.innerHTML = 'PLAY';
-playButton.style.display = 'block';
-playButton.addEventListener('click', clickHandler, false);
-
-document.body.appendChild(playButton);
-
-function logEvent(e) {
-	console.log(e.type);
-}
-
-function clickHandler() {
-	video.fullscreen().play().pause().play();
-}
+	.play()
+	.pause()
+	.fullscreen()
+	.load([
+		{ src: 'videos/my_other_really_cool_video.mp4', type: 'video/mp4' },
+		{ src: 'videos/my_other_really_cool_video.ogv', type: 'video/ogg' },
+		{ src: 'videos/my_other_really_cool_video.webm', type: 'video/webm' }
+	]);
