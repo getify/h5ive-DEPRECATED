663ba8057ceb7e6e3bb4ecff89f1a669151e0f6e
Kyle Simpson <getify@gmail.com>
2012-08-23 14:11:55 -0500

    first pass at 'animationFrame' module

--- animationFrame.h5ive.js
+++ animationFrame.h5ive.js
@@ -4,13 +4,77 @@
 
 	if (!h5) throw new Error("animationFrame.h5ive: core.h5ive required.");
 
-	h5.animationFrame = function(opts) {
-		var publicAPI;
+	var rAF = (window.msRequestAnimationFrame || window.mozRequestAnimationFrame ||
+			window.webkitRequestAnimationFrame || window.oRequestAnimationFrame
+		),
+		cAF = (window.msCancelAnimationFrame || window.msCancelRequestAnimationFrame ||
+			window.mozCancelAnimationFrame || window.mozCancelRequestAnimationFrame ||
+			window.webkitCancelAnimationFrame || window.webkitCancelRequestAnimationFrame ||
+			window.oCancelAnimationFrame || window.oCancelRequestAnimationFrame
+		),
+		publicAPI, q_ids = {}
+	;
 
-		publicAPI = {
-		};
+	function qID(){
+		var id;
+		do {
+			id = Math.floor(Math.random() * 1E9);
+		} while (id in q_ids);
+		return id;
+	}
 
+	function queue(cb) {
+		var qid = qID();
+
+		q_ids[qid] = rAF(function(){
+			delete q_ids[qid];
+			cb.apply(publicAPI,arguments);
+		});
+
+		return qid;
+	}
+
+	function queueAfter(cb) {
+		var qid;
+
+		qid = queue(function(){
+			// do our own rAF call here because we want to re-use the same `qid` for both frames
+			q_ids[qid] = rAF(function(){
+				delete q_ids[qid];
+				cb.apply(publicAPI,arguments);
+			});
+		});
+
+		return qid;
+	}
+
+	function cancel(qID) {
+		if (qID in q_ids) {
+			cAF(q_ids[qID]);
+			delete q_ids[qID];
+		}
 		return publicAPI;
-	};
+	}
+
+	function unsupported() {
+		throw new Error("'requestAnimationFrame' not supported.");
+	}
+
+	if (rAF && cAF) {
+		publicAPI = {
+			queue: queue,
+			queueAfter: queueAfter,
+			cancel: cancel
+		};
+	}
+	else {
+		publicAPI = {
+			queue: unsupported,
+			queueAfter: unsupported,
+			cancel: unsupported
+		};
+	}
+
+	h5.animationFrame = publicAPI;
 
 })(this.h5);
--- example-animationFrame.js
+++ example-animationFrame.js
@@ -1,9 +1,36 @@
-var aFrame = h5.animationFrame();
+var aFrame = h5.animationFrame,
+	body = document.body,
+	text, id1, id2, id3
+;
 
-aFrame
-.current(function(){
-	// ...
-})
-.next(function(){
-	// ...
+id1 = aFrame.queue(function(){
+	// canceled, won't ever get called
+	text = document.createTextNode("##");
+	body.appendChild(text);
 });
+id2 = aFrame.queueAfter(function(){
+	// canceled, won't ever get called
+	text = document.createTextNode("!!");
+	body.appendChild(text);
+});
+id3 = aFrame.queueAfter(function(){
+	// canceled, won't ever get called
+	text = document.createTextNode("$$");
+	body.appendChild(text);
+});
+
+aFrame.queueAfter(function(){
+	text = document.createTextNode("third.");
+	body.appendChild(text);
+});
+aFrame.queue(function(){
+	aFrame.cancel(id3); // still time to cancel something before the next frame
+	text = document.createTextNode("second.");
+	body.appendChild(text);
+});
+
+aFrame.cancel(id1);
+aFrame.cancel(id2);
+
+text = document.createTextNode("first.");
+body.appendChild(text);
